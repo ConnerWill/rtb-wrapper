30cff174e39b9daa786a49150c61106a12c17791
ConnerWill <connerwill@connerwill.com>
2022-11-26 19:19:50 -0600

    Fixed shellcheck errors, added color output functions

--- rtb-wrapper.sh
+++ rtb-wrapper.sh
@@ -3,22 +3,25 @@
 set -o nounset   ## set -u : exit the script if you try to use an uninitialised variable
 set -o errexit   ## set -e : exit the script if any statement returns a non-true return value
 
+fn_color_print_error(){   printf "\x1B[0;1;38;5;196m%s\x1B[0m\n" "${@}"; }
+fn_color_print_warning(){ printf "\x1B[0;38;5;190m%s\x1B[0m\n"   "${@}"; }
+fn_color_print_success(){ printf "\x1B[0;38;5;46m%s\x1B[0m\n"    "${@}"; }
+fn_color_print_verbose(){ printf "\x1B[0;38;5;87m%s\x1B[0m\n"    "${@}"; }
+
+
 # Print CLI usage help
 fn_display_usage () {
-	echo "Usage: rtb-wrapper.sh <action> <profile>"
-	echo ""
-	echo "action: backup, restore"
-	echo "profile: name of the profile file"
-	echo ""
-	echo "For more detailed help, please see the README file:"
-	echo ""
-	echo "https://github.com/thomas-mc-work/rtb-wrapper/blob/master/README.md"
+	printf "Usage: rtb-wrapper.sh <action> <profile>\n\n"
+	printf "action: backup, restore\n"
+	printf "profile: name of the profile file\n\n"
+	printf "For more detailed help, please see the README file:\n\n"
+	printf "https://github.com/thomas-mc-work/rtb-wrapper/blob/master/README.md\n"
 }
 
 # create backup cli command
 fn_create_backup_cmd () {
-
-  cmd="rsync_tmbackup.sh '${SOURCE}' '${TARGET}'"
+  #shellcheck disable=2154
+  cmd="${rsync_tmbackup_path} '${SOURCE}' '${TARGET}'"
 
   exclude_file_check=${EXCLUDE_FILE:-}
 
@@ -26,7 +29,7 @@ fn_create_backup_cmd () {
     cmd="${cmd} '${EXCLUDE_FILE}'"
   fi
 
-  echo "$cmd"
+  echo "${cmd}"
 }
 
 # create restore cli command
@@ -39,11 +42,11 @@ fn_create_restore_cmd () {
 
     cmd="${cmd} '${TARGET}/latest/' '${SOURCE}/'"
 
-    echo "$cmd"
+    echo "${cmd}"
 }
 
 fn_abort_if_crlf () {
-  if ! awk '/\r$/ { exit(1) }' "$1"; then
+  if ! awk '/\r$/ { exit(1) }' "${1}"; then
     echo " [!] The profile has at least one Windows-style line ending"
     echo "     ERROR: failed to read the profile file: ${profile_file}" > /dev/stderr
     exit 1
@@ -67,34 +70,34 @@ profile_dir="${config_dir}/conf.d"
 profile_file="${profile_dir}/${profile}.inc"
 exclude_file_convention="${profile_dir}/${profile}.excludes.lst"
 
-if [ -r "$profile_file" ]; then
+if [ -r "${profile_file}" ]; then
   # preset exclude file path before reading the profile
-  if [ -r "$exclude_file_convention" ]; then
-    EXCLUDE_FILE="$exclude_file_convention"
+  if [ -r "${exclude_file_convention}" ]; then
+    EXCLUDE_FILE="${exclude_file_convention}"
   fi
 
   # sanity check, crlf can break variable substitution
-  fn_abort_if_crlf "$profile_file"
-  # shellcheck disable=SC1090,SC1091
-  . "$profile_file"
+  fn_abort_if_crlf "${profile_file}"
+  # ellcheck disable=SC1090,SC1091
+  . "${profile_file}"
 
   # Check if command is found
   rsync_tmbackup_cmd="rsync_tmbackup.sh"
   rsync_tmbackup_path=$(command -v "${rsync_tmbackup_cmd}" 2>/dev/null)
-  if [ ! "${rsync_tmbackup_path}" ]; then
+  if [ -z "${rsync_tmbackup_path}" ]; then
     printf "Cannot find command:\t%s" "${rsync_tmbackup_cmd}"
     exit 1
   fi
 
   # create cli command
-  if [ "$action" = "restore" ]; then
+  if [ "${action}" = "restore" ]; then
     cmd=$(fn_create_restore_cmd)
   else
     cmd=$(fn_create_backup_cmd)
   fi
 
-  #echo "# ${cmd}"
-  eval "$cmd"
+  echo "# ${cmd}"
+  eval "${cmd}"
 else
   echo "Failed to read the profile file: ${profile_file}" > /dev/stderr
   exit 1
